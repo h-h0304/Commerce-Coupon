bae8cb4d871974a60593729f572a4ce4bc93fc11
liz <fgnhu76@ajou.ac.kr>
2025-06-09 15:42:38 +0900

    init: Spring Boot 프로젝트로 초기화 + 기존 README 유지

---
+++ src/test/java/com/commercecoupon/commercecouponbe/CommerceCouponBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commercecoupon.commercecouponbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceCouponBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
